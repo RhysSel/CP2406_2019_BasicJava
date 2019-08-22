7499ba42dbdfe9147d3700d6e84673750aa8d3d9
jc321565 <rhys.selwyn@my.jcu.edu.au>
2019-08-22 17:30:01 +1000

    Initial code for Dog class,setup Junit5

---
+++ src/DogTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class DogTest {
+
+    void testDefault() {
+        Dog dog = new Dog();
+        assertNull(dog.name);
+        assertNull(dog.breed);
+        assertNull(dog.shotStatus);
+        assertEquals(0, dog.age);
+    }
+}
